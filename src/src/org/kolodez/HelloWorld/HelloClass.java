5c62839b19859feb3ac3ca070f0e2fbdcf61eb7b
Kolodez2106 <74856521+Kolodez2106@users.noreply.github.com>
2020-12-18 18:24:42 +0100

    Create HelloClass.java

---
+++ src/src/org/kolodez/HelloWorld/HelloClass.java
@@ -0,0 +1,13 @@
+package org.kolodez.HelloWorld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloClass extends Activity {
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        TextView tv = (TextView) this.findViewById (R.id.MainTextView);
+        tv.setText ("Hello, world!");
+    }   
+}
